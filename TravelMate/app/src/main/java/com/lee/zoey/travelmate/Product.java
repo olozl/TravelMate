9abb50f81a3402dad5e5e4d7729ab97842a10a1c
unknown <leeeun@pdx.edu>
2017-07-11 23:09:48 -0700

    open source

---
+++ TravelMate/app/src/main/java/com/lee/zoey/travelmate/Product.java
@@ -0,0 +1,27 @@
+package com.lee.zoey.travelmate;
+
+/**
+ * Created by user1 on 2017-07-11.
+ */
+
+import android.graphics.drawable.Drawable;
+
+import android.graphics.drawable.Drawable;
+
+public class Product {
+
+    public String title;
+    public Drawable productImage;
+    public String description;
+    public double price;
+    public boolean selected;
+
+    public Product(String title, Drawable productImage, String description,
+                   double price) {
+        this.title = title;
+        this.productImage = productImage;
+        this.description = description;
+        this.price = price;
+    }
+
+}
---
+++ TravelMate/app/src/main/java/com/lee/zoey/travelmate/ShoppingCartEntry.java
@@ -0,0 +1,29 @@
+package com.lee.zoey.travelmate;
+
+/**
+ * Created by user1 on 2017-07-11.
+ */
+
+public class ShoppingCartEntry {
+
+    private Product mProduct;
+    private int mQuantity;
+
+    public ShoppingCartEntry(Product product, int quantity) {
+        mProduct = product;
+        mQuantity = quantity;
+    }
+
+    public Product getProduct() {
+        return mProduct;
+    }
+
+    public int getQuantity() {
+        return mQuantity;
+    }
+
+    public void setQuantity(int quantity) {
+        mQuantity = quantity;
+    }
+
+}
